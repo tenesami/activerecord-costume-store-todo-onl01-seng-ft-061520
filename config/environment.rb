c3b77058ca0b5e41fbd36c77964dbcec77a58cd3
Tesfaye Atomsa <tesfayeatomsa@gmail.com>
2020-07-21 05:06:08 -0600

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -1,12 +1,20 @@
-require "bundler/setup"
+require 'bundler/setup'
+Bundler.require
 
-require "sinatra/activerecord"
+require 'rake'
+require 'active_record'
+require 'yaml/store'
 require 'ostruct'
 require 'date'
 
-Bundler.require
+DBNAME = "halloween"
 
 Dir[File.join(File.dirname(__FILE__), "../app/models", "*.rb")].each {|f| require f}
+Dir[File.join(File.dirname(__FILE__), "../lib/support", "*.rb")].each {|f| require f}
 
-connection_details = YAML::load(File.open('config/database.yml'))
-ActiveRecord::Base.establish_connection(connection_details)
+DBRegistry[ENV["ACTIVE_RECORD_ENV"]].connect!
+DB = ActiveRecord::Base.connection
+
+if ENV["ACTIVE_RECORD_ENV"] == "test"
+  ActiveRecord::Migration.verbose = false
+end
